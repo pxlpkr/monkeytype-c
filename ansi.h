3b35e9796c46fb268b1fab769b11ec57b8e213e9
pxlpkr <pixelparkour@parkkila.net>
2023-02-02 23:36:49 -0800

    random prompt generator, keypress evaluation

---
+++ ansi.h
@@ -0,0 +1,3 @@
+#define ANSI_RED "[31m"
+#define ANSI_WHITE "[37m"
+#define ANSI_GRAY "[90m"
--- main.c
+++ main.c
@@ -2,10 +2,18 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <ctype.h>
+#include <ncurses.h>
+#include <string.h>
+
+#include "ansi.h"
+#include "lang.h"
 
 static const char FLAGS[] = "t:";
+
+static int padding = 4;
 static int time_limit = 15;
 
+/* Convert a string into an integer value */
 int make_int(char* str) {
     char *end;
     return strtol(str, &end, 10);
@@ -20,9 +28,7 @@ void resolve_args(int argc, char** argv) {
 
         int c = getopt_long(argc, argv, FLAGS, long_options, &option_index);
 
-        if (c == -1) {
-            break;
-        }
+        if (c == -1) break;
 
         switch (c) {
             case 't':
@@ -38,8 +44,95 @@ void resolve_args(int argc, char** argv) {
     }
 }
 
+void make_line(char* string, int len) {
+    while (1) {
+        const char *word = *(english_1k + rand() % english_1k_length);
+
+        if ((int) strlen(string) + (int) strlen(word) + 1 >= len) return;
+
+        strcat(string, word);
+        strcat(string, " ");
+    }
+}
+
+void init_ncurses() {
+    initscr();
+    start_color();
+    use_default_colors();
+
+    init_pair(1, COLOR_BLACK, -1);
+    init_pair(2, COLOR_WHITE, -1);
+    init_pair(3, COLOR_RED, -1);
+
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    scrollok(stdscr, FALSE);
+}
+
 int main(int argc, char** argv) {
     resolve_args(argc, argv);
+    
+    init_ncurses();
+
+    const int LINEWIDTH = getmaxx(stdscr) - 2 * padding;
+    char prompts[3][LINEWIDTH];
+    strcpy(prompts[0], "");
+
+    for (int i = 0; i < 3; i++) {
+        char string[LINEWIDTH + 1];
+        memset(string, 0, sizeof(char) * (LINEWIDTH + 1));
+        make_line(string, LINEWIDTH + 1);
+        strcpy(prompts[i], string);
+    }
+
+    attron(COLOR_PAIR(1));
+    for (int i = 0; i < 3; i++) {
+        printw("%-4s%s\n", "", prompts[i]);
+    }
+    attroff(COLOR_PAIR(1));
+    move(0, padding);
+
+    int x_pos = 0;
+    int y_pos = 0;
+
+    while (1) {
+        move(y_pos, x_pos + padding);
+        int c = getch();
+
+        if (c != -1) {
+            char cur_char = prompts[y_pos][x_pos];
+            if (c == 127) {
+                attron(COLOR_PAIR(1));
+                printw("%c", cur_char);
+                attroff(COLOR_PAIR(1));
+                x_pos--;
+            } else {
+                if (c == cur_char) {
+                    attron(COLOR_PAIR(2));
+                    printw("%c", cur_char);
+                    attroff(COLOR_PAIR(2));
+                } else {
+                    attron(COLOR_PAIR(3));
+                    printw("%c", cur_char);
+                    attroff(COLOR_PAIR(3));
+                }
+                x_pos++;
+            }
+
+            if (x_pos >= (int) strlen(prompts[y_pos])) {
+                y_pos++;
+                x_pos = 0;
+            } else if (x_pos < 0) {
+                if (y_pos > 0) {
+                    y_pos--;
+                    x_pos = (int) strlen(prompts[y_pos]) - 1;
+                } else {
+                    x_pos = 0;
+                }
+            }
+        }
+    }
 
     return 0;
 }
